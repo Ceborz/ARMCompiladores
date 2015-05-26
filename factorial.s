d455781bc7701d5c9d8f02872b74ea67e1e34bd5
Ceborz <richie.zepeda@gmail.com>
2015-05-25 23:28:57 -0600

    nuevo archivo

--- factorial.s
+++ factorial.s
@@ -73,7 +73,7 @@ _condExpFalse_0:
 	pop {R4}
 	pop {R6}
 	STR R6, [R11, #0]
-	MUL R4, R4, R5
+	MUL R4, R5, R4
 	pop {R5}
 	push {R4}
 	MOV pc, R5
@@ -97,10 +97,6 @@ main_0:
 	pop {R5}
 	STR R5, [R11, #0]
 	STR R4, [R11, #0]
-	LDR R4, [R11, #0]
-	LDR R0, =_formatoInt
-	MOV R1, R4
-	BL printf
 	pop {pc}
 
 _IndexOutOfBounds:
