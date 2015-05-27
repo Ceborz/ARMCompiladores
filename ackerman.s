8d8a00941df49a8919d9dab161e4ec2da7f0b8d4
Ceborz <richie.zepeda@gmail.com>
2015-05-26 18:20:57 -0600

    nuevo archivo

--- ackerman.s
+++ ackerman.s
@@ -8,6 +8,39 @@ main:
 	BL main_0
 	B _salir
 
+DivideU32:
+	result .req r0
+	remainder .req r1
+	shift .req r2
+	current .req r3
+	clz shift,r1
+	clz r3,r0
+	subs shift,r3
+	lsl current,r1,shift
+	mov remainder,r0
+	mov result,#0
+	blt divideU32Return$
+	divideU32Loop$:
+		cmp remainder,current
+		blt divideU32LoopContinue$
+		add result,result,#1
+		subs remainder,current
+		lsleq result,shift
+		beq divideU32Return$
+	divideU32LoopContinue$:
+		subs shift,#1
+		lsrge current,#1
+		lslge result,#1
+		bge divideU32Loop$
+divideU32Return$:
+	.unreq current
+	mov pc,lr
+	.unreq result
+	.unreq remainder
+	.unreq shift
+
+	
+
 ackermann_0:
 	pop {R4}
 	STR R4, [R11, #0]
@@ -43,11 +76,11 @@ _condExpTrue_1:
 	push {R4}
 	LDR R4, [R11, #8]
 	push {R4}
+	MOV R4, #1
+	push {R4}
 	LDR R4, [R11, #0]
 	MOV R5, #1
 	SUB R4, R4, R5
-	push {R4}
-	MOV R4, #1
 	push {R4}
 	BL ackermann_0
 	pop {R4}
@@ -69,11 +102,11 @@ _condExpFalse_1:
 	push {R4}
 	LDR R4, [R11, #8]
 	push {R4}
-	LDR R4, [R11, #0]
-	push {R4}
 	LDR R4, [R11, #4]
 	MOV R5, #1
 	SUB R4, R4, R5
+	push {R4}
+	LDR R4, [R11, #0]
 	push {R4}
 	BL ackermann_0
 	pop {R4}
@@ -90,11 +123,11 @@ _condExpFalse_1:
 	push {R4}
 	LDR R4, [R11, #8]
 	push {R4}
+	LDR R4, [R11, #8]
+	push {R4}
 	LDR R4, [R11, #0]
 	MOV R5, #1
 	SUB R4, R4, R5
-	push {R4}
-	LDR R4, [R11, #8]
 	push {R4}
 	BL ackermann_0
 	pop {R4}
@@ -121,9 +154,9 @@ main_0:
 	push {R4}
 	LDR R4, [R11, #8]
 	push {R4}
-	LDR R4, [R11, #4]
-	push {R4}
 	LDR R4, [R11, #8]
+	push {R4}
+	LDR R4, [R11, #4]
 	push {R4}
 	BL ackermann_0
 	pop {R4}
